84c55de9a60c4a46fa9a1c46526fc43fd68fe52c
Carlos Navarrete <navarretecarp@globalhitss.com>
2022-06-08 18:35:29 -0500

    Deploy, Events, fix images assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { PlaceDetailsPageComponent } from './pages/domain/places/place-details-page/place-details-page.component';
 import { ArtistRidersListPageComponent } from './pages/domain/riders/artist-riders-list-pages/artist-riders-list-page.component';
 import { ArtistRiderDetailsPageComponent } from './pages/domain/riders/artist-rider-details-page/artist-rider-details-page.component';
+import { EventsListPageComponent } from './pages/domain/events/events-list-page/events-list-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'artists', component: ArtistsListPageComponent },
   { path: 'artist/:id', component: ArtistDetailsPageComponent },
+  { path: 'events/:artistId', component: EventsListPageComponent },
   { path: 'place/:id', component: PlaceDetailsPageComponent },
   { path: 'riders/:artistId', component: ArtistRidersListPageComponent },
   { path: 'riders/:artistId/:riderId', component: ArtistRiderDetailsPageComponent },

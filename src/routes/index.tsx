21279074d72c2206c152abcb1902d0167135531e
npcarlos <npcarlos@users.noreply.github.com>
2022-10-03 23:37:57 -0500

    Creación de deploy automático (#3)
    * Compilación de cosas nuevas
    
    * FTP Upload

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
-import {lazy} from "react";
-import {Routes, Route, Navigate} from "react-router-dom";
+import { lazy } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import {PATHS, SUB_PATHS, URL_PARAMETER_NAMES} from "../constants";
+import { PATHS, SUB_PATHS, URL_PARAMETER_NAMES } from "../constants";
 
 // Lazy loading
 const HomePage = lazy(() => import("../components/Pages/HomePage"));
@@ -9,18 +9,30 @@ const NotFoundPage = lazy(() => import("../components/Pages/NotFoundPage"));
 const SearchPage = lazy(() => import("../components/Pages/SearchPage"));
 
 // Load rider pages
-const RiderListPage = lazy(() => import("../components/Pages/RidersPage/RiderList"));
+const RiderListPage = lazy(
+  () => import("../components/Pages/RidersPage/RiderList")
+);
 
-const RiderDetailPage = lazy(() => import("../components/Pages/RidersPage/RiderDetail"));
+const RiderDetailPage = lazy(
+  () => import("../components/Pages/RidersPage/RiderDetail")
+);
 
 // Load events/shows pages
-const ShowListPage = lazy(() => import("../components/Pages/ShowsPage/ShowsList"));
+const ShowListPage = lazy(
+  () => import("../components/Pages/ShowsPage/ShowsList")
+);
 
-const ShowDetailPage = lazy(() => import("../components/Pages/ShowsPage/ShowDetail"));
+const ShowDetailPage = lazy(
+  () => import("../components/Pages/ShowsPage/ShowDetail")
+);
 
 // Load Artists pages
-const ArtistsListPage = lazy(() => import("../components/Pages/ArtistsPage/ArtistsList"));
-const ArtistDetailPage = lazy(() => import("../components/Pages/ArtistsPage/ArtistDetail"));
+const ArtistsListPage = lazy(
+  () => import("../components/Pages/ArtistsPage/ArtistsList")
+);
+const ArtistDetailPage = lazy(
+  () => import("../components/Pages/ArtistsPage/ArtistDetail")
+);
 
 export const RoutesApp: React.FC = () => {
   return (
@@ -29,21 +41,27 @@ export const RoutesApp: React.FC = () => {
         <Route element={<ArtistsListPage />} path="" />
         <Route
           element={<ArtistDetailPage />}
-          path={SUB_PATHS.DETAIL + `/:${URL_PARAMETER_NAMES.ELEMENT_ID}`}
+          path={
+            SUB_PATHS.ELEMENT_DETAILS + `/:${URL_PARAMETER_NAMES.ELEMENT_ID}`
+          }
         />
       </Route>
       <Route path={PATHS.RIDERS}>
         <Route element={<RiderListPage />} path="" />
         <Route
           element={<RiderDetailPage />}
-          path={SUB_PATHS.DETAIL + `/:${URL_PARAMETER_NAMES.ELEMENT_ID}`}
+          path={
+            SUB_PATHS.ELEMENT_DETAILS + `/:${URL_PARAMETER_NAMES.ELEMENT_ID}`
+          }
         />
       </Route>
       <Route path={PATHS.SHOWS}>
         <Route element={<ShowListPage />} path="" />
         <Route
           element={<ShowDetailPage />}
-          path={SUB_PATHS.DETAIL + `/:${URL_PARAMETER_NAMES.ELEMENT_ID}`}
+          path={
+            SUB_PATHS.ELEMENT_DETAILS + `/:${URL_PARAMETER_NAMES.ELEMENT_ID}`
+          }
         />
       </Route>
       <Route element={<HomePage />} path={PATHS.HOME} />
